8085f8f4bf8f0c6da8cf425decc985e5eeab6957
Kimginam97 <rlarlska79@naver.com>
2023-12-12 17:35:51 +0900

    메인페이지 추가

--- app/page.js
+++ app/page.js
@@ -1,113 +1,83 @@
 import Image from 'next/image'
+import styles from './home.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-white">
+      {/* 라임색 배경 */}
+      <div className="absolute w-full h-[321px] left-0 top-[759px] bg-lime-500" />
+
+      {/* 하단 안내 문구 */}
+      <div className="w-[1042px] h-[27px] left-[439px] top-[868px] absolute text-center text-white text-base font-medium font-['Gmarket Sans TTF']">
+        * 본 성과는 [2023년도 디지털 식품정보 플랫폼 구축] 사업의 성과입니다.
+      </div>
+
+      {/* 로고 */}
+      <Image
+        className="w-[217px] h-[133px] left-[66px] top-[75px] absolute"
+        src="/ui/logo.png"
+        alt="logo"
+        width={213}
+        height={133}
+      />
+
+      {/* 중앙 텍스트 */}
+      <div className="w-[900px] h-[58px] left-[513px] top-[284px] text-center absolute">
+        <span className="text-black text-5xl font-bold font-['Gmarket Sans TTF']">
+          디지털트윈
+        </span>
+        <span className="text-black text-5xl font-medium font-['Gmarket Sans TTF']">
+          으로 만나는 국가식품클러스터
+        </span>
+      </div>
+
+      {/* 압출성형기 섹션 */}
+      <section className="w-[422px] h-[416px] left-[494px] top-[397px] absolute">
+        <div className="w-full h-full absolute flex-col justify-start items-start flex">
+          <div className={styles.logoContainer}>
+            <Link href="/extruder-system">
+              <Image
+                src="/ui/img-ext.png"
+                alt="img-ext"
+                width={422}
+                height={271}
+              />
+              <div className={styles.hoverContent}>바로가기</div>
+            </Link>
+          </div>
+          <div className="w-full h-[345px] bg-white rounded-[20px] shadow border border-white" />
+          <div className="w-[183px] h-[27px] left-[119px] top-[297px] absolute text-center text-black text-3xl font-bold font-['Gmarket Sans TTF']">
+            압출성형기
+          </div>
+          <div className="w-[254px] h-[27px] left-[84px] top-[344px] absolute text-center text-zinc-600 text-[26px] font-medium font-['Gmarket Sans TTF']">
+            파일럿 플랜트
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* PET 라인 섹션 */}
+      <section className="w-[422px] h-[416px] left-[1005px] top-[397px] absolute">
+        <div className="w-full h-full absolute bg-white rounded-[20px] shadow border border-white" />
+        <div className="w-[183px] h-[27px] left-[119px] top-[297px] absolute text-center text-black text-3xl font-bold font-['Gmarket Sans TTF']">
+          PET 라인
+        </div>
+        <div className="w-[254px] h-[27px] left-[84px] top-[344px] absolute text-center text-zinc-600 text-[26px] font-medium font-['Gmarket Sans TTF']">
+          기능성식품 제형센터
+        </div>
+        <div className={styles.logoContainer}>
+          <Link href="/pet-line">
+            <Image
+              className="w-full h-[271px]"
+              src="/ui/img-pet.png"
+              alt="img-pet"
+              width={422}
+              height={271}
+            />
+            <div className={styles.hoverContent}>바로가기</div>
+          </Link>
+        </div>
+      </section>
+    </div>
   )
 }
---
+++ app/pet-line/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Page = () => {
+  return <div>Pet-line</div>
+}
+
+export default Page
